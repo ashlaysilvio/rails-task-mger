266f26157f217ca163cabab9bd7e723b373dcdba
ashlay silvio <asilvio@morganphilips.com>
2023-01-23 16:33:36 +0400

    task model created

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_23_121952) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_23_123207) do
   create_table "tasks", force: :cascade do |t|
+    t.string "title"
+    t.text "details"
+    t.boolean "completed"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
